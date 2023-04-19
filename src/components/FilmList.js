73ebec4a87533b3592822212a81253dd400f3809
aprilkrg <aprilkrgonzales@gmail.com>
2023-04-19 13:21:46 -0600

    step 5 complete - mapping over films in FilmList

--- src/components/FilmList.js
+++ src/components/FilmList.js
@@ -2,11 +2,16 @@ import { Component } from 'react'
 
 export default class FilmList extends Component {
     render() {
+        const allFilms = this.props.films.map((film) => {
+            return (<div className="film-row">
+                <h1>{film.title}</h1>
+            </div>)
+        })
         return (
             <>
                 <div className="film-list">
                     <h1 className="section-title">FILMS</h1>
-                    <h2>{this.props.films[0].title}</h2>
+                    {allFilms}
                 </div>
             </>
         )
