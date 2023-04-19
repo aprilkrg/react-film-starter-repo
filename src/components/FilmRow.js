3b65f3c9e3607224be104567fef14620de7d72eb
aprilkrg <aprilkrgonzales@gmail.com>
2023-04-19 14:23:39 -0600

    step 7 complete - show film poster and year

--- src/components/FilmRow.js
+++ src/components/FilmRow.js
@@ -2,10 +2,15 @@ import { Component } from 'react'
 
 export default class FilmRow extends Component {
     render() {
+        const posterUrl = "https://image.tmdb.org/t/p/w780/" + this.props.film.poster_path
         return (
             <>
                 <div className="film-row">
-                    <h1>{this.props.film.title}</h1>
+                    <img src={posterUrl} alt="" />
+                    <div className="film-summary">
+                        <h1>{this.props.film.title}</h1>
+                        <p>{this.props.film.release_date.split("-")[0]}</p>
+                    </div>
                 </div>
             </>
         )
