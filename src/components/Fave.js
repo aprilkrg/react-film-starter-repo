7b2849f894f5c9b5a08cd673fdce6ce90b7ee78a
aprilkrg <aprilkrgonzales@gmail.com>
2023-04-19 17:26:42 -0600

    goal 4 complete - setting isFave state

--- src/components/Fave.js
+++ src/components/Fave.js
@@ -1,18 +1,24 @@
 import { Component } from "react";
 
 export default class Fave extends Component {
+    state = {
+        isFave: false
+    }
+
     handleClick = (e) => {
         e.stopPropagation()
         console.log("handling fave click!")
+        this.setState({isFave: !this.state.isFave})
     }
+
     render() {
         return (
             <>
                 <div 
-                    className="film-row-fave add_to_queue" 
+                    className={`film-row-fave ${this.state.isFave === true ?"remove_from_queue" : "add_to_queue"}`}
                     onClick={this.handleClick}
                 >
-                    <p className="material-icons">add_to_queue</p>
+                    <p className="material-icons">{`${this.state.isFave === true ?"remove_from_queue" : "add_to_queue"}`}</p>
                 </div>
             </>
         )
